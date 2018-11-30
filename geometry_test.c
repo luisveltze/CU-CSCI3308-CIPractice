47026f3c698b917d967dd9a4b2d130439c1dee07
luisveltze <luis.veltze@colorado.edu>
2018-11-30 11:21:10 -0700

    updated travis file

--- geometry_test.c
+++ geometry_test.c
@@ -172,6 +172,59 @@ Suite* coord_2d_suite(void)
 
 }
 
+/* coord_2d_area_triangle Test */
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    
+    a.x = b.x = 0;
+    a.y = b.y = 0;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 0;
+    exp.y = 0;
+    ck_assert(coord_2d_eq(&mid, &exp));
+    
+    a.x = 0;
+    a.y = 0;
+    b.x = 3;
+    b.y = 0;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 1.5;
+    exp.y = 0;
+    ck_assert(coord_2d_eq(&mid, &exp));
+    
+    a.x = 0;
+    a.y = 0;
+    b.x = 0;
+    b.y = 3;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 0;
+    exp.y = 1.5;
+    ck_assert(coord_2d_eq(&mid, &exp));
+    
+    a.x = 0;
+    a.y = 0;
+    b.x = 3;
+    b.y = 3;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 1.5;
+    exp.y = 1.5;
+    ck_assert(coord_2d_eq(&mid, &exp));
+    
+    a.x = 1;
+    a.y = 2;
+    b.x = 3;
+    b.y = 4;
+    coord_2d_midpoint(&mid, &a, &b);
+    exp.x = 2;
+    exp.y = 3;
+    ck_assert(coord_2d_eq(&mid, &exp));
+    
+}
+END_TEST
+
 /* main: run test suites and set exit status */
 int main(void){
 
